a680ee422e56e3c7e12cdde9995671cfdbf2b860
Greg Stephen <GregAStephen@gmail.com>
2019-03-18 19:15:57 -0500

    got the convert button ticket done

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log("hey");
+let tempInput = document.getElementById("tempInput");
+const fBtn = document.getElementById("fahrenheit");
+const cBtn = document.getElementById("celsius");
+const convertBtn = document.getElementById("convertBtn");
+
+
+const printToDom = (divId, textToPrint) => {
+    let selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += textToPrint;
+};
+
+const determineConverter = () => {
+    console.log("hey");
+    console.log(tempInput.value); 
+    if(cBtn.checked) {
+        console.log("C");
+    } else if (fBtn.checked) {
+        console.log("F");
+    };
+    printToDom("tempOutput", tempInput.value)
+};
+
+convertBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    determineConverter();
+    });
+
