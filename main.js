f792bd47f7ce59e1d562a4f4422f9632ede6f0d8
Greg Stephen <GregAStephen@gmail.com>
2019-03-18 19:51:44 -0500

    got the temp converter working

--- main.js
+++ main.js
@@ -5,20 +5,42 @@ const convertBtn = document.getElementById("convertBtn");
 const clearBtn = document.getElementById("clearBtn");
 let tempOutput = document.getElementById("tempOutput");
 
+
 const printToDom = (divId, textToPrint) => {
     let selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML += textToPrint;
+    selectedDiv.innerHTML = textToPrint;
 };
 
-const determineConverter = () => {
+const domStringBuilder = (finalTemp, unit) => {
+    let domString = "";
+    domString += `<h2>`;
+        domString += `${finalTemp} `;
+        domString += `degrees `;
+        domString += `${unit}`; 
+    domString += `</h2>`;
+    printToDom("tempOutput", domString);
+};
+const toCelsius = (temp) => {
+    let convertedTemp = (((temp - 32) * 5) / 9);
+    domStringBuilder(convertedTemp, "C");
+};
+
+const toFahrenheit = (temp) => {
+    let convertedTemp = (((temp * 9) / 5) + 32);
+    domStringBuilder(convertedTemp, "F");
+};
+
+const determineConverter = (temp) => {
     console.log("hey");
-    console.log(tempInput.value); 
+    console.log(temp); 
     if(cBtn.checked) {
         console.log("C");
+        toCelsius(temp);
     } else if (fBtn.checked) {
         console.log("F");
+        toFahrenheit(temp);
     };
-    printToDom("tempOutput", tempInput.value)
+    // printToDom("tempOutput", tempInput.value)
 };
 
 const clear = () => {
@@ -28,7 +50,7 @@ const clear = () => {
 
 convertBtn.addEventListener('click', function(e){
     e.preventDefault();
-    determineConverter();
+    determineConverter(tempInput.value);
     });
 
 clearBtn.addEventListener('click', function(){
